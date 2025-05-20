53e6710ef11f6aadf3403117f0b593b5ca8f7af9
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-05-20 12:41:29 +0200

    0.822 for logistic regression

--- notebooks/pipeline_functions.py
+++ notebooks/pipeline_functions.py
@@ -1,6 +1,11 @@
 import pandas as pd
+from typing import Callable
 
-def exclude_spaces(dataframe : pd.DataFrame, column_name: str ) -> pd.DataFrame:
-    new_dataframe = dataframe.copy()
-    new_dataframe[column_name] = new_dataframe[column_name].replace(' ', 0).astype('float64')
-    return new_dataframe
+def exclude_spaces(column_name: str) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    
+    def transform(dataframe: pd.DataFrame) -> pd.DataFrame:
+        new_dataframe = dataframe.copy()
+        new_dataframe[column_name] = new_dataframe[column_name].replace(' ', 0).astype('float64')
+        return new_dataframe
+    
+    return transform
