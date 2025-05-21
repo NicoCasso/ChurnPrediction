8d18512786d8342db009ca7d13bd09edcce9a22f
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-05-21 10:08:25 +0200

    re-add notebooks

--- src/pipeline_functions.py
+++ src/pipeline_functions.py
@@ -51,12 +51,41 @@ def create_preprocessor() -> Pipeline :
 
 import tensorflow as tf
 
+class F1Score(tf.keras.metrics.Metric):
+    def __init__(self, name='f1_score', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.tp = self.add_weight(name='tp', initializer='zeros')
+        self.fp = self.add_weight(name='fp', initializer='zeros')
+        self.fn = self.add_weight(name='fn', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.round(y_pred)
+
+        tp = tf.reduce_sum(tf.cast(y_true * y_pred, tf.float32))
+        fp = tf.reduce_sum(tf.cast((1 - y_true) * y_pred, tf.float32))
+        fn = tf.reduce_sum(tf.cast(y_true * (1 - y_pred), tf.float32))
+
+        self.tp.assign_add(tp)
+        self.fp.assign_add(fp)
+        self.fn.assign_add(fn)
+
+    def result(self):
+        precision = self.tp / (self.tp + self.fp + tf.keras.backend.epsilon())
+        recall = self.tp / (self.tp + self.fn + tf.keras.backend.epsilon())
+        return 2 * (precision * recall) / (precision + recall + tf.keras.backend.epsilon())
+
+    def reset_states(self):
+        self.tp.assign(0)
+        self.fp.assign(0)
+        self.fn.assign(0)
+
 def build_nn_model(X_train:pd.DataFrame) -> tf.keras.Model :
     
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(X_train.shape[1],)),
         tf.keras.layers.Dense(64, activation='sigmoid'),
-        tf.keras.layers.Dense(16, activation='sigmoid'),
+        tf.keras.layers.Dense(32, activation='sigmoid'),
         tf.keras.layers.Dense(16, activation='sigmoid'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
@@ -64,7 +93,13 @@ def build_nn_model(X_train:pd.DataFrame) -> tf.keras.Model :
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
         loss='binary_crossentropy',
-        metrics=['accuracy', 'AUC', 'recall']
+        metrics=[
+            tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+            tf.keras.metrics.Precision(name='precision'),
+            #tf.keras.metrics.Recall(name='recall'),
+            tf.keras.metrics.AUC(name='roc_auc')
+            #F1Score()  # Custom metric
+        ]
     )
 
     model.summary()
